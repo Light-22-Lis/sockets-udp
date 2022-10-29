d6b58cc886e050f871022416e880808fb79befcb
AsriFox <asrifox@yandex.ru>
2022-10-29 21:25:46 +0300

    Refactor with high-level statements

--- cs-sockets-udp/Program.cs
+++ cs-sockets-udp/Program.cs
@@ -1,69 +1,63 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading;
 
-namespace UdpClientApp
+string GetLine() => Console.ReadLine() ?? throw new OperationCanceledException("Cancelled");
+System.Text.Encoding encoding = System.Text.Encoding.Unicode;
+
+try {
+    Console.Write("Enter the remote address to connect to:\t");
+    string remoteAddress = GetLine();
+    Console.Write("Enter the connection (remote) port:\t\t");
+    int remotePort = int.Parse(GetLine());
+    Console.Write("Enter the listening (local) port:\t\t");
+    int localPort = int.Parse(GetLine());
+
+    Thread receiveThread = new(new ParameterizedThreadStart(ReceiveMessage));
+    receiveThread.Start(localPort);
+    SendMessage(remoteAddress, remotePort);
+}
+catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+}
+
+void SendMessage(string remoteAddress, int remotePort)
 {
-    class Program
-    {
-        static string remoteAddress;
-        static int remotePort, localPort;
-        static void Main(string[] args)
-        {
-            try{
-                Console.Write("Enter the listening port: ");
-                localPort = Int32.Parse(Console.ReadLine());
-                Console.Write("Enter the remote address to connect to: ");
-                remoteAddress = Console.ReadLine();
-                Console.Write("Enter the connection port: ");
-                remotePort = Int32.Parse(Console.ReadLine());
-
-                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
-                receiveThread.Start();
-                SendMessage();
-            }
-            catch(Exception exception){
-                Console.WriteLine(exception.Message);
-            }
+    UdpClient sender = new();
+    try {
+        while(true) {
+            string message = GetLine();
+            byte[] data = encoding.GetBytes(message);
+            sender.Send(data, data.Length, remoteAddress, remotePort);
         }
-        private static void SendMessage()
-        {
-            UdpClient sender = new UdpClient();
-            try{
-                while(true){
-                    string message = Console.ReadLine();
-                    byte[] data = Encoding.Unicode.GetBytes(message);
-                    sender.Send(data, data.Length, remoteAddress, remotePort);
-                }
-            }
-            catch(Exception exception){
-                Console.WriteLine(exception.Message);
-            }
-            finally{
-                sender.Close();
-            }
-        }
-        private static void ReceiveMessage()
-        {
-            UdpClient receiver = new UdpClient(localPort);
-            IPEndPoint remoteIp = null;
-            try{
-                while(true){
-                    byte[] data = receiver.Receive(ref remoteIp);
-                    string message = Encoding.Unicode.GetString(data);
-                    Console.Write("Enter your name: ");
-                    string name = Console.ReadLine();
-                    Console.WriteLine("{0}: {1}",name, message);
-                }
-            }
-            catch(Exception exception){
-                Console.WriteLine(exception.Message);
-            }
-            finally{
-                receiver.Close();
-            }
-        }        
+    }
+    catch (Exception ex) {
+        Console.WriteLine(ex.Message);
+    }
+    finally{
+        sender.Close();
     }
 }
+        
+void ReceiveMessage(object? arg)
+{
+    if (arg is not int localPort)
+        throw new InvalidCastException();
+
+    UdpClient receiver = new(localPort);
+    IPEndPoint? remoteIp = null;
+    try {
+        while (true) {
+            byte[] data = receiver.Receive(ref remoteIp);
+            string message = encoding.GetString(data);
+            Console.Write("Enter your name: ");
+            string name = GetLine();
+            Console.WriteLine("{0}: {1}",name, message);
+        }
+    }
+    catch(Exception exception){
+        Console.WriteLine(exception.Message);
+    }
+    finally{
+        receiver.Close();
+    }
+} 
