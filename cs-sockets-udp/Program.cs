e6a63a2cf35538405efe812fa3222b28d5fcd52e
AsriFox <asrifox@yandex.ru>
2022-11-02 15:28:57 +0300

    Try multicast

--- cs-sockets-udp/Program.cs
+++ cs-sockets-udp/Program.cs
@@ -4,30 +4,35 @@ using System.Net.Sockets;
 string GetLine() => Console.ReadLine() ?? throw new OperationCanceledException("Cancelled");
 System.Text.Encoding encoding = System.Text.Encoding.Unicode;
 
+IPAddress remoteAddress = IPAddress.Parse("224.0.0.1");
+
 try {
-    Console.Write("Enter the remote address to connect to:\t");
-    string remoteAddress = GetLine();
-    Console.Write("Enter the connection (remote) port:\t");
-    int remotePort = int.Parse(GetLine());
-    Console.Write("Enter the listening (local) port:\t");
-    int localPort = int.Parse(GetLine());
+    //Console.Write("Enter the remote address to connect to:\t");
+    //Console.Write("Enter the connection (remote) port:\t");
+    int remotePort = 8001;// int.Parse(GetLine());
+    //Console.Write("Enter the listening (local) port:\t");
+    int localPort = 8001;// int.Parse(GetLine());
+
+    Console.Write("Your name: ");
+    string userName = GetLine();
 
     Thread receiveThread = new(new ParameterizedThreadStart(ReceiveMessage));
     receiveThread.Start(localPort);
-    SendMessage(remoteAddress, remotePort);
+    SendMessage(remotePort, userName);
 }
 catch (Exception ex) {
     Console.WriteLine(ex.Message);
 }
 
-void SendMessage(string remoteAddress, int remotePort)
+void SendMessage(int remotePort, string userName)
 {
     UdpClient sender = new();
+    IPEndPoint remoteEp = new(remoteAddress, remotePort);
     try {
         while(true) {
-            string message = GetLine();
+            string message = $"{userName}: {GetLine()}";
             byte[] data = encoding.GetBytes(message);
-            sender.Send(data, data.Length, remoteAddress, remotePort);
+            sender.Send(data, data.Length, remoteEp);
         }
     }
     catch (Exception ex) {
@@ -44,12 +49,16 @@ void ReceiveMessage(object? arg)
         throw new InvalidCastException();
 
     UdpClient receiver = new(localPort);
+    receiver.JoinMulticastGroup(remoteAddress, 20);
     IPEndPoint? remoteIp = null;
+    IPAddress localAddress = LocalIPAddress();
     try {
         while (true) {
             byte[] data = receiver.Receive(ref remoteIp);
+            if (remoteIp.Address == localAddress)
+                continue;
             string message = encoding.GetString(data);
-            Console.WriteLine("Other: " + message);
+            Console.WriteLine(message);
         }
     }
     catch(Exception ex) {
@@ -59,3 +68,12 @@ void ReceiveMessage(object? arg)
         receiver.Close();
     }
 } 
+
+IPAddress? LocalIPAddress()
+{
+    IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+    foreach (IPAddress ip in host.AddressList)
+        if (ip.AddressFamily == AddressFamily.InterNetwork)
+            return ip;
+    return null;
+}
